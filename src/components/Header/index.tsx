496b69e9a2961460ad7db9c54acddbfd54c95109
leonardomessias98 <messiasleonardo98@gmail.com>
2021-07-29 21:10:44 -0300

    feat(shared_componenets): creating header, signInButton and SubscribeButton

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,20 @@
+import Image from "next/image";
+import logo from "../../public/images/logo.svg";
+
+import SignInButton from "../SignInButton";
+import styles from "./styles.module.scss";
+
+export default function Header() {
+  return (
+    <header className={styles.headerContainer}>
+      <div className={styles.headerContent}>
+        <Image src={logo} alt="logo" />
+        <nav>
+          <a className={styles.active}>Home</a>
+          <a>Posts</a>
+        </nav>
+        <SignInButton />
+      </div>
+    </header>
+  );
+}
